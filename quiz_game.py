5f239e48f076be7892c073ec4f088f5dfc3075ee
begad-land <125161870+begad-land@users.noreply.github.com>
2023-02-22 15:00:49 -0800

    v3: completed the game

--- quiz_game.py
+++ quiz_game.py
@@ -1,31 +1,53 @@
 questions=[
-    'whats 2+2',
-    'how many fingers in the human hand'
+    'whats the coldest planet in our solar system?',
+    'what is the second most spoken language in the world?',
+    'what is the name of the second Greek number?',
+    'what is the name of a shape with 7 angles?',
+    'when a living organism makes more of its own, what is this proccess called?',
+    'what is the name of the species of human beings?',
+    'in which country is the longest river in the world?'
 ]
 
 choices=[
-    ['A.4 ', 'B.5' ,'C.10', 'D.3'],
+    ['A. Uranus ', 'B. Earth' ,'C. Jupiter', 'D. Mercury'],
 
-    ['A.1 ', 'B.3' ,'C.9', 'D.5']
+    ['A. French', 'B. English' ,'C. Hindi', 'D. Mandrin Chinese'],
+
+    ['A. Eth', 'B. Tria' , 'C. Dio' ,'D. Tessera'],
+
+    ['A. Heptagon' , 'B. Octagon', 'C. pentagon' , 'D. Hexagon'],
+
+    ['A. Reproduction', 'B. Production' , 'C. Increase' , 'D. Reduction' ],
+
+    ['A. Amphibians', 'B. Homo sapiens', 'C. Mammals', 'D.Humans' ],
+
+    ['A. France' , 'B. Egypt', 'C. U.S.A', 'D.Brazil' ],
     ]
-answers=['A','D']
+
+answers=['A', 'D', 'C' ,'A', 'A', 'B' , 'B' ]
 
 C_no=0  
 Q_no=1
+score=0
 for question in questions:
     print(f'Q{Q_no}: {question}')
     Q_no+=1
 
     for choice in choices[C_no]:
         print(choice)
+        print('-----')
+
     answer=input('pick A/B/C/D ' ).upper()
+
     if answer==answers[C_no]:
         print('CORRECT')
+        score+=1
     else:
         print('INCORRECT')
+        score-=1
         print(f'the answer is : {answers[C_no]}')
     print('---------------------')
-
     C_no+=1
-    
+print('----Result----')
+print(f'your score: {score}\nyou got {score} out of {len(choices)} correct')
 
