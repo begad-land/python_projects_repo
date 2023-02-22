23754608209558b6c62eda2a53ccd90d06987272
begad-land <125161870+begad-land@users.noreply.github.com>
2023-02-22 14:02:25 -0800

    version 2: added the answer feature

--- quiz_game.py
+++ quiz_game.py
@@ -8,15 +8,24 @@ choices=[
 
     ['A.1 ', 'B.3' ,'C.9', 'D.5']
     ]
+answers=['A','D']
+
 C_no=0  
 Q_no=1
 for question in questions:
     print(f'Q{Q_no}: {question}')
     Q_no+=1
-    
+
     for choice in choices[C_no]:
         print(choice)
+    answer=input('pick A/B/C/D ' ).upper()
+    if answer==answers[C_no]:
+        print('CORRECT')
+    else:
+        print('INCORRECT')
+        print(f'the answer is : {answers[C_no]}')
     print('---------------------')
+
     C_no+=1
     
 
