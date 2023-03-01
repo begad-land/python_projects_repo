37fea831f98e744e3b6c971de9b16b3c910378fc
begad-land <125161870+begad-land@users.noreply.github.com>
2023-03-01 14:21:22 -0800

    fixed bug with correction,telling how many qs were

--- quiz_game_OOPv.py
+++ quiz_game_OOPv.py
@@ -6,6 +6,7 @@ class Question:
         C_num=0
         A_num=0
         score=0
+        Num_of_Qs=len(self.kwargs['questions'])
         for q in self.kwargs['questions']:
             print(q)
 
@@ -21,27 +22,27 @@ class Question:
             else:
                 print(f'INCORRECT \nThe correct answer: {self.kwargs["answers"][A_num]}')
                 score - 1
-                A_num+=1
+            A_num+=1
             print('-----------------------------')
-        print(f'Your score: {score}pts') 
+        print(f'Your score: {score}/{Num_of_Qs}pts') 
                 
-q=Question(questions=[
+q=Question(questions=(
                       'whats the coldest planet in our solar system?', 
                       'what is the second most spoken language in the world?' ,
                       'what is the most abudnant element in the atmosphere',
                       'what is the name of a shape with 7 angles?',
                       'when a living organism makes more of its own, what is this proccess called?',
                       'what is the name of the species of human beings?',
-                      'in which country is the longest river in the world?'], 
-            choices=[
-                    ['A. Uranus', 'B. Earth' ,'C. Jupiter', 'D. Mercury'],
-                    ['A. French', 'B. English' ,'C. Hindi', 'D. Mandrin Chinese'],
-                    ['A.Francium(Fr)', 'B. Oxygen(O2)', 'C. Nitrogen(N2)', 'D.Hydrogen(H)' ], 
-                    ['A. Heptagon' , 'B. Octagon', 'C. pentagon' , 'D. Hexagon'],
-                    ['A. Reproduction', 'B. Production' , 'C. Increase' , 'D. Reduction'],
-                    ['A. Amphibians', 'B. Homosapiens', 'C. Mammals', 'D.Humans'],
-                    ['A. France' , 'B. Egypt', 'C. U.S.A', 'D.Brazil' ]
-                    ],
-            answers=['A', 'D', 'C' ,'A', 'A', 'B' , 'B' ]
+                      'in which country is the longest river in the world?'), 
+            choices=(
+                    ('A. Uranus', 'B. Earth' ,'C. Jupiter', 'D. Mercury'),
+                    ('A. French', 'B. English' ,'C. Hindi', 'D. Mandrin Chinese'),
+                    ('A.Francium(Fr)', 'B. Oxygen(O2)', 'C. Nitrogen(N2)', 'D.Hydrogen(H)' ), 
+                    ('A. Heptagon' , 'B. Octagon', 'C. pentagon' , 'D. Hexagon'),
+                    ('A. Reproduction', 'B. Production' , 'C. Increase' , 'D. Reduction'),
+                    ('A. Amphibians', 'B. Homosapiens', 'C. Mammals', 'D.Humans'),
+                    ('A. France' , 'B. Egypt', 'C. U.S.A', 'D.Brazil' )
+                    ),
+            answers=('A', 'D', 'C' ,'A', 'A', 'B' , 'B')
             )
 q.display_Qs()
