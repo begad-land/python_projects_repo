ea762ce6a2f8580f7ce8fc6bbe49def2d86d2f87
begad-land <125161870+begad-land@users.noreply.github.com>
2023-04-06 13:32:09 +0200

    only score calculation is left

--- quiz_game_OOPv.py
+++ quiz_game_OOPv.py
@@ -1,54 +1,65 @@
 
-class Question:
-    def __init__(self,**kwargs) -> None:
-        self.kwargs=kwargs
-        
+import random
+
+class Game:
+    def __init__(self,Q,Qs_saved) -> None:
+        self.Q=Q
+        self.Qs_saved=Qs_saved
+
     def display_Qs(self):
-        C_num=0
-        A_num=0
         score=0
-        Num_of_Qs=len(self.kwargs['questions'])
-        for q in self.kwargs['questions']:
-            print(q)
 
-            for x in self.kwargs['choices'][C_num]:
-                print(f'{x}\n---------------------------')
-            C_num+=1
+        print(f'{self.Qs_saved}) {self.Q[0]}\n------------------------------------------------------')
 
-            guess=input('pick A/B/C/D ').upper()
-            if guess==self.kwargs['answers'][A_num]:
-                print('CORRECT')
-                score+=1     
+        for i in self.Q[1][0]:
+            print(f'{i}\n-------------------------------------- ') 
 
-            else:
-                print(f'INCORRECT \nThe correct answer: {self.kwargs["answers"][A_num]}')
-                score - 1
-            A_num+=1
-            print('-----------------------------')
-        print(f'Your score: {score}/{Num_of_Qs}pts') 
-                
-q=Question(questions=(
-                      'whats the coldest planet in our solar system?', 
-                      'what is the second most spoken language in the world?' ,
-                      'what is the most abudnant element in the atmosphere',
-                      'what is the name of a shape with 7 angles?',
-                      'when a living organism makes more of its own, what is this proccess called?',
-                      'what is the hardest substance on Earth',
-                      'in which country is the longest river in the world?',
-                      'which planet has the most moons?',
-                      'How many elements in the periodic table?'
-                      ), 
-            choices=(
-                    ('A. Uranus', 'B. Earth' ,'C. Jupiter', 'D. Mercury'),
-                    ('A. French', 'B. English' ,'C. Hindi', 'D. Mandrin Chinese'),
-                    ('A.Francium(Fr)', 'B. Oxygen(O2)', 'C. Nitrogen(N2)', 'D.Hydrogen(H)' ), 
-                    ('A. Heptagon' , 'B. Octagon', 'C. pentagon' , 'D. Hexagon'),
-                    ('A. Reproduction', 'B. Production' , 'C. Increase' , 'D. Reduction'),
-                    ('A. Platinum', 'B. Titamnium', 'C. Diamond', 'D. Iron'),
-                    ('A. France' , 'B. Egypt', 'C. U.S.A', 'D.Brazil' ),
-                    ('A. Jupiter', 'B. Mars', 'C. Mercury', 'D. Saturn'),
-                    ('A. 125', 'B. 118', 'C. 117', 'D. 123')
-                    ),
-            answers=('A', 'D', 'C' ,'A', 'A', 'C' ,'B', 'D', 'B')
-            )
-q.display_Qs()
+        guess=input('pick A/B/C/D ').upper()
+
+        if guess==self.Q[1][1]:
+            print(f'correct\n')
+            score+=1
+
+        else:
+            print(f'false the correct answer is: {self.Q[1][1]}\n')
+            
+        
+
+s= {
+                      'whats the coldest planet in our solar system?':(('A. Uranus', 'B. Earth' ,'C. Jupiter', 'D. Neptune'),'A'),
+
+                      'what is the second most spoken language in the world?':(('A. French', 'B. English' ,'C. Hindi', 'D. Mandrin Chinese'),'D'),
+                      
+                      'what is the most abudnant element in the atmosphere':(('A. Francium(Fr)', 'B. Oxygen(O2)', 'C. Nitrogen(N2)', 'D. Hydrogen(H)' ),'C'),
+
+                      'what is the name of a shape with 7 angles?':(('A. Heptagon' , 'B. Octagon', 'C. pentagon' , 'D. Hexagon'),'A'),
+
+                      'when a living organism makes more of its own, what is this proccess called?':(('A. Reproduction', 'B. Production' , 'C. Increase' , 'D. Reduction'),'A'),
+
+                      'what is the hardest substance on Earth':(('A. Platinum', 'B. Titamnium', 'C. Diamond', 'D. Iron'),'C'),
+
+                      'in which country is the longest river in the world?':(('A. France' , 'B. Egypt', 'C. U.S.A', 'D. Brazil' ),'B'),
+
+                      'which planet has the most moons?': (('A. Jupiter', 'B. Mars', 'C. Mercury', 'D. Saturn'),'D'),
+
+                      'How many elements are in the periodic table?':(('A. 125', 'B. 118', 'C. 117', 'D. 123'),'B'),
+
+                      'What is the country that has the second highest population':(('A. India', 'B. U.S.A', 'C. Indonesia' ,'D. China'), 'A'),
+
+                      'What is the biggest organ in the human body':(('A.Liver', 'B. Brain', 'C. Skin', 'D. Lungs'),'C')
+                    }
+
+Qs_saved=[]
+while len(Qs_saved) < 7:
+    
+    randQs=random.choice(list(s.items()))
+
+    if randQs in Qs_saved:
+        continue
+
+    Qs_saved.append(randQs)
+
+    elements=Game(randQs,len(Qs_saved))
+
+    elements.display_Qs() 
+print(elements.score)
