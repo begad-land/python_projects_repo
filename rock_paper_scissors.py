820c16dd0f26c37f1bf2b76b2c4c18e232aaa0f9
begad-land <125161870+begad-land@users.noreply.github.com>
2023-06-22 00:23:58 +0300

    some changes

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,24 +7,26 @@ p_score=0
 c_score=0
 while True:
     computer=random.choice(choices)
-    player=input('pick (rock/ paper / scissors ) ')
+    player=input('pick (rock/ paper / scissors ) ').lower()
+    print('----------------------------------------------------------')
 
 
     while player not in choices:
         player=input('pick (rock/ paper / scissors ) ')
 
     if player[0]=='r' and computer[0]=='s' or  player[0]=='p' and computer[0]=='r' or player[0]=='s' and computer[0]=='p':
-        print(f'\nthe computer picked {computer} and you picked {player}  \nyou win!')
+        print(f'\nthe computer picked {computer}\nyou picked {player}  \nyou win!')
         p_score+=1
         
     elif player[0]==computer[0]:
-        print(f'\nthe computer picked {computer} and you picked {player}  \nits a tie!')
+        print(f'\nthe computer picked {computer}\nyou picked {player}  \nits a tie!')
 
     else:
-        print(f'\nthe computer picked {computer} and you picked {player}  \nyou lose!')
+        print(f'\nthe computer picked {computer}\nyou picked {player}  \nyou lose!')
         c_score+=1
     if input('play again y/n ') !='y':
         break
+    print('------------------------------------------------------------------')
     
 print(f'\nyour score: {p_score}\nthe computers score: {c_score}')
 print('thanks for playing')
