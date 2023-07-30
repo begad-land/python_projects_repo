6e6e84fc8eb06a227c8fc8a39fba7eb0e952b2ba
begad-land <125161870+begad-land@users.noreply.github.com>
2023-07-30 15:06:05 +0300

    added sizes

--- ordering project .py
+++ ordering project .py
@@ -1,3 +1,5 @@
+#this is temporary. if a user picks the same item more than once but with diffrent sizes try to append that items size inside the dict "cart"
+
 import time
 print('\nOFFER DAY!!\norder 4 items or more to get 10% off your order ')
 menu={
@@ -22,19 +24,20 @@ menu={
     '7':{'item':'Ice Cream', 
         'price':3.47},
     }
-print('-------------------MENU---------------------------\n item                   price\n')
+print('====================MENU========================\n   item                 price\n')
 
 counter=1
 for pick in menu.values():
     print(f'| {counter}) {pick["item"]:16} | {pick["price"]:1}$ |')
 
     counter+=1
-print('-----------------------------------------------')
+print('======================================================')
 
 class Check:
-    def __init__(self,order,amount) -> None:
+    def __init__(self,order,items,sizes) -> None:
         self.order=order
-        self.amount=amount
+        self.items=items
+        self.sizes=sizes
 
     def calc(self):
         total=0
@@ -43,26 +46,28 @@ class Check:
         discounted_percentage= 10 / 100 * total
         tax= total * 0.15
 
-        print('\n-------------------------order summary-----------------------\n')
+        print('\n=====================order summary=========================\n')
 
-        if self.amount >= 4:
+        if len(self.items) >= 4:
             print(f'10% discount granted!\n')
             for key, value in self.order.items():
-                print(f'Item: {key} {value[1]}X | Price: {value[0]}$\n----------------------------------------------')
-            print(f'\nTotal: {round(total - discounted_percentage,2)}\nTax: {round(tax,2)}')
+                print(f'Item: {key:16} {value[1]}X | Price: {value[0]}$\n=========================================================')
+            print(f'\nTotal: {round(total - discounted_percentage,2)}\nTax: {round(tax,2)}\n===================================')
 
         else:
             for key, value in self.order.items():
-                print(f'Item: {key} {value[1]}X | Price {value[0]}$\n----------------------------------------------')
-            print(f'\nTotal: {round(total,2)}\nTax: {round(tax,2)}')
+                print(f'Item: {key:16} {value[1]}X | Price {value[0]}$\n===========================================================')
+            print(f'\nTotal: {round(total,2)}\nTax: {round(tax,2)}\n============================================================')
         print()
+        for i , s in zip(self.items, self.sizes):
+            print(f'items: {i:16} | sizes: {s}')
+
+        print('=================================================')
         when=time.localtime()
-        print('----------------------------------------------------') 
         Time=time.strftime('%B %d %Y %I%p:%M:%S', when )
         print(f'date of the order:\n{Time}')
-        print('----------------------------------------------------')
+        print('=================================================')
         
-
 
 items=[]
 
@@ -70,17 +75,18 @@ cart={}
 
 while True: 
     picks=list(input('\npick the number of the item u want (click enter to checkout) ').split())
-    sizes=list(input('pick a size depending on the order if your items (L/M/S)').split())
+    sizes=list(input('pick a size depending on the order of your items (L/M/S)').upper().split())
 
     for pick in picks:
       if pick not in menu:
           continue
-      
-      cart[menu[pick]['item']]=[round(menu[pick]['price'] * picks.count(pick),2) ,picks.count(pick)]  
-      items.append(menu[pick]['item'])
+        
+      cart[menu[pick[0]]['item']] = [round(menu[pick[0]]['price'] * picks.count(pick),2) , picks.count(pick)]
+
+      items.append(menu[pick[0]]['item'])
 
     break
 
-#have the sizes in a list and allign with each item using a an index var 
-order=Check(cart,len(items))
+
+order=Check(cart, items,sizes)
 order.calc()
