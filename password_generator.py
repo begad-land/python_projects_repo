48d290ea497869bae1bb81e26314620a19d3a7a0
begad-land <125161870+begad-land@users.noreply.github.com>
2023-08-09 00:00:34 +0300

    infinite password generation

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,16 @@
 
+# sourcery skip: for-append-to-extend, simplify-generator
 import random
 import string
+import time
 
 letters1=list(string.ascii_uppercase)
 
 nums=['1','2','3','4','5','6','7','8','9']
 
-for i in nums:
-    letters1.append(i)
-
-password=[]
-
-for x in range(8):
-    password.append(random.choice(letters1))
-
-print(''.join(password))
+while True:
+    for i in nums:
+        letters1.append(i)
+    password = [random.choice(letters1) for x in range(8)]
+    print(''.join(password))
+    time.sleep(6)
