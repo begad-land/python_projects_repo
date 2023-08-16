7b81d4a5099bca6123333254dd47476fa640ad64
begad-land <125161870+begad-land@users.noreply.github.com>
2023-08-16 13:03:34 +0300

    added symbols

--- password_generator.py
+++ password_generator.py
@@ -8,9 +8,20 @@ letters1=list(string.ascii_uppercase)
 
 nums=['1','2','3','4','5','6','7','8','9']
 
+symbols=['@','$','!','&','*']
+
 while True:
     for i in nums:
         letters1.append(i)
+
+    for symbol in symbols:
+        letters1.append(symbol)    
+    
     password = [random.choice(letters1) for x in range(8)]
+
+    for i in symbols:
+        if i not in password:
+            continue
+        
     print(''.join(password))
     time.sleep(6)
