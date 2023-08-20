35a139677e17edeacddb31a0028db859638b6b83
begad-land <125161870+begad-land@users.noreply.github.com>
2023-08-20 23:53:44 +0300

    added the discount feature

--- ordering project pandas.py
+++ ordering project pandas.py
@@ -2,8 +2,6 @@
 import time as tm
 import pandas as pd
 
-
-
 class Total:
     def __init__(self,lst_prices=[]) -> None:
         self.lst_prices=lst_prices
@@ -18,26 +16,34 @@ class Items:
     def __init__(self,lst_items=[]) -> None:
         self.lst_items=lst_items
 
-
-
 class Invoice:
     def __init__(self,order) -> None:
         self.order=order
         self.total=Total(self.order[k][0] for k, v in self.order.items())
         self.items=Items(self.order[k] for k in self.order)
         
-
     def invoice_presentaion(self):
+        amount_of_items=0
+        fees= self.total.calc_total() * 0.15
+
+        for k, v in self.order.items():
+            amount_of_items+=v[1]
+
         print()
         for k, v in self.order.items():
             print(f'Item: {k:5} {v[1]}X | Price: {v[0]}$\n\n------------------------------------------')
 
-        print(f'Total:{round(self.total.calc_total(),2)}$\n------------------------------------------')
+        if amount_of_items >= 4:
+            discounted_percentage= 10 / 100 * self.total.calc_total()
+            print(f'10% DISCOUNT GRANTED!')
+            print(f'Total: {round(self.total.calc_total() - discounted_percentage),2}$\n------------------------------------------')
+        else:
+            print(f'Total: {round(self.total.calc_total(),2)}$\n------------------------------------------')
+
+        print(f'fees: {round(fees),2}$')
         when=tm.localtime()
         Time=tm.strftime('%B %d %Y %I%p:%M:%S', when )
         print(f'date of the order:\n{Time}')    
-
-           
 
 menu={
     'foods':['pizza','steak','burger','Mango juice','mushroom soup'],
@@ -47,6 +53,7 @@ menu_df=pd.DataFrame(menu)
 menu_df.index=['1','2','3','4','5']
 
 print(f'-------------MENU-----------\n{menu_df}')
+
 lst_of_nums=['1','2','3','4','5']
 
 order={}
