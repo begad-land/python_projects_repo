1d7df5e7f5af4aadf926d602ca055e7b928322ab
begad-land <125161870+begad-land@users.noreply.github.com>
2023-09-17 00:09:29 +0300

    added paument method classes

--- ordering project pandas.py
+++ ordering project pandas.py
@@ -2,6 +2,38 @@
 import time as tm
 import pandas as pd
 
+class Cash:
+    def cash_status(self):
+        print(f'payment: cash')
+
+class Debit:
+   def debit_status(self):
+        print(f'payment: debit ')
+        debit_no=str(input('insert your debit no '))
+
+        while len(debit_no) !=7:
+            print('ERROR: there are still missing numbers')
+            debit_no=input('insert your debit no ')
+
+        print(f'your debit number is ****{debit_no[4::]}')
+
+class Pay:
+    def __init__(self,) -> None:
+        self.cash=Cash()
+        self.debit=Debit()
+
+    def getting_payment_method(self):
+        
+        print(f'1)cash\n2)debit\n ')
+
+        method=input('how would you like to pay? ')
+
+        if method in ['1', 'cash']:
+            self.cash.cash_status()
+
+        elif method in ['2', 'debit']:
+            self.debit.debit_status()
+
 #calculating the total
 class Total:
     def __init__(self,lst_prices=[]) -> None:
@@ -72,6 +104,9 @@ while True:
                                           round(menu_df.loc[pick]['prices'] * picks.count(pick),2), picks.count(pick)
                                           ] 
     break
+
+pay=Pay()
+pay.getting_payment_method()
 
 invoice=Invoice(order)
 invoice.invoice_presentaion()
