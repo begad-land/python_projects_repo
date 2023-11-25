a1208d0faacca4d29b220e9f627db1acb20c6882
begad-land <125161870+begad-land@users.noreply.github.com>
2023-11-25 22:28:05 +0200

    excplicitly saying which symbols i want

--- password_generator.py
+++ password_generator.py
@@ -11,22 +11,21 @@ nums=['1','2','3','4','5','6','7','8','9']
 
 symbols=['@','$','&','*','#']
 
-pattern=re.compile(r'[0-9]+[A-Z]+[^A-Za-z0-9]{1,2}')
+
+pattern=re.compile(r'[0-9]+[A-Z]+[@#%&*]{1,3}')
 
 while True:
     for i in nums:
         letters1.append(i)
 
     for symbol in symbols:
-        letters1.append(symbol)    
+        letters1.append(symbol) 
 
-    
     password = [random.choice(letters1) for x in range(9)]
     password=''.join(password)
 
     if pattern.search(password) is None:
         continue
-
 
     print(password)    
     time.sleep(6)
