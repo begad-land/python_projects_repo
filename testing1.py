65b87e6e0b754c0ead38f7d776bec985c383bbb3
begad-land <125161870+begad-land@users.noreply.github.com>
2024-04-09 16:55:22 +0200

    testing

--- testing1.py
+++ testing1.py
@@ -1,2 +1,23 @@
 
+x = {
+    'movies':{
+        'a':[
+            [1,2,3,4,5],'hello'
+            ]
+    },
+    'b':[
+            [6,7,8,9,10],'world'
+            ]
+}
 
+nums = [10, 4,2]
+
+
+
+
+
+
+for letter , number in x['movies'].items():
+    for num in nums:
+        if num in number[0]:
+            number[0].remove(num)
